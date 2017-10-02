8b7e67f96de13bc58051f18a40d50b399eeee2c8
johannw <johannwestphal@gmail.com>
2017-10-02 11:37:57 -0300

    Renomeando arquivo

--- app/main_fingerprint.cc
+++ app/main_fingerprint.cc
@@ -14,8 +14,8 @@ int getFingerprintIDez();
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
 OStream cout;
-UART mySerial{9600, 8, 0, 1, 1};
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+UART *mySerial;
+Adafruit_Fingerprint *finger;
 
 // On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
 //Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
@@ -26,15 +26,15 @@ void setup()
   cout << "Adafruit finger detect test" << "\n";
 
   // set the data rate for the sensor serial port
-  finger.begin(57600);
+  finger->begin(57600);
 
-  if (finger.verifyPassword()) {
+  if (finger->verifyPassword()) {
     cout << "Found fingerprint sensor!" << "\n";
   } else {
     cout << "Did not find fingerprint sensor :(" << "";
     while (1);
   }
-  cout << "Waiting for valid finger..." << "\n";
+  cout << "Waiting for valid finger->.." << "\n";
 }
 
 void loop()                     // run over and over again
@@ -44,7 +44,7 @@ void loop()                     // run over and over again
 }
 
 uint32_t getFingerprintID() {
-  uint32_t p = finger.getImage();
+  uint32_t p = finger->getImage();
   switch (p) {
     case FINGERPRINT_OK:
       cout << "Image taken" << "\n";
@@ -65,7 +65,7 @@ uint32_t getFingerprintID() {
 
   // OK success!
 
-  p = finger.image2Tz();
+  p = finger->image2Tz();
   switch (p) {
     case FINGERPRINT_OK:
       cout << "Image converted" << "\n";
@@ -88,7 +88,7 @@ uint32_t getFingerprintID() {
   }
 
   // OK converted!
-  p = finger.fingerFastSearch();
+  p = finger->fingerFastSearch();
   if (p == FINGERPRINT_OK) {
     cout << "Found a print match!" << "\n";
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
@@ -103,29 +103,31 @@ uint32_t getFingerprintID() {
   }
 
   // found a match!
-  cout << "Found ID #" << finger.fingerID << "\n";
-  cout << " with confidence of " << finger.confidence << "\n";
+  cout << "Found ID #" << finger->fingerID << "\n";
+  cout << " with confidence of " << finger->confidence << "\n";
 }
 
 // returns -1 if failed, otherwise returns ID #
 int getFingerprintIDez() {
-  uint32_t p = finger.getImage();
+  uint32_t p = finger->getImage();
   if (p != FINGERPRINT_OK)  return -1;
 
-  p = finger.image2Tz();
+  p = finger->image2Tz();
   if (p != FINGERPRINT_OK)  return -1;
 
-  p = finger.fingerFastSearch();
+  p = finger->fingerFastSearch();
   if (p != FINGERPRINT_OK)  return -1;
 
   // found a match!
-  cout << "Found ID #" << finger.fingerID << "\n";
-  cout << " with confidence of " << finger.confidence << "\n";
-  return finger.fingerID;
+  cout << "Found ID #" << finger->fingerID << "\n";
+  cout << " with confidence of " << finger->confidence << "\n";
+  return finger->fingerID;
 }
 
 int main()
 {
+  mySerial = new UART(0, 9600, 8, 0, 1);
+  finger = new Adafruit_Fingerprint(mySerial);
   setup();
   loop();
   return 0;
